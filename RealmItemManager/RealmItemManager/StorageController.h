5ff2dcdb6bf0f871fd29b9da647f245b87f8af28
Weien Wang <weienw@gmail.com>
2016-08-10 11:16:29 -0500

    [NEW] StorageController

---
+++ RealmItemManager/RealmItemManager/StorageController.h
@@ -0,0 +1,13 @@
+//
+//  StorageController.h
+//  RealmItemManager
+//
+//  Created by Weien Wang on 8/10/16.
+//  Copyright © 2016 Weien Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StorageController : NSObject
+
+@end
---
+++ RealmItemManager/RealmItemManager/StorageController.m
@@ -0,0 +1,13 @@
+//
+//  StorageController.m
+//  RealmItemManager
+//
+//  Created by Weien Wang on 8/10/16.
+//  Copyright © 2016 Weien Wang. All rights reserved.
+//
+
+#import "StorageController.h"
+
+@implementation StorageController
+
+@end
