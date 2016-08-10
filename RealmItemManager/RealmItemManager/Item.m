8700ff0a5c9086bfab3100e730143eb7fd6bff4a
Weien Wang <weienw@gmail.com>
2016-08-10 11:05:42 -0500

    [ADD] requiredProperties to Item and Note

--- RealmItemManager/RealmItemManager/Item.m
+++ RealmItemManager/RealmItemManager/Item.m
@@ -17,18 +17,8 @@
              };
 }
 
-// Specify default values for properties
-
-//+ (NSDictionary *)defaultPropertyValues
-//{
-//    return @{};
-//}
-
-// Specify properties to ignore (Realm won't persist these)
-
-//+ (NSArray *)ignoredProperties
-//{
-//    return @[];
-//}
++ (NSArray *)requiredProperties {
+    return @[@"content"];
+}
 
 @end
--- RealmItemManager/RealmItemManager/Note.m
+++ RealmItemManager/RealmItemManager/Note.m
@@ -10,18 +10,8 @@
 
 @implementation Note
 
-// Specify default values for properties
-
-//+ (NSDictionary *)defaultPropertyValues
-//{
-//    return @{};
-//}
-
-// Specify properties to ignore (Realm won't persist these)
-
-//+ (NSArray *)ignoredProperties
-//{
-//    return @[];
-//}
++ (NSArray *)requiredProperties {
+    return @[@"content", @"item"];
+}
 
 @end
